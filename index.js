64f58b52daf2041d4d5f26e59d8ff031a00c23e1
leo <166565836+leouax@users.noreply.github.com>
2024-08-27 15:27:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ import { privateKeyToAccount } from "viem/accounts";
 import { mainnet } from "viem/chains";
 import { toBytes } from "viem";
 import { generatePrivateKey } from "viem/accounts";
+import cors from 'cors';
+import express from 'express'
+const app = express();
+const port = 8000;
+
+const urlToGroupId = {'amazon.com': 'fe2200f01ad87a7576d363a1db800518'}
 
 // Function to send a message to a specific group
 async function sendMessageToGroup(client, groupId, messageContent) {
@@ -16,6 +22,24 @@ async function sendMessageToGroup(client, groupId, messageContent) {
   }
   await conversation.send(messageContent);
   console.log(`Message sent to group ${groupId}: ${messageContent}`);
+}
+
+export default async function sendMsgExternal(wallet, url, messageContent) {
+  if (!fs.existsSync(`.cache`)) {
+      fs.mkdirSync(`.cache`);
+    }
+  const client = await setupClient(wallet, {
+      dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+  });
+  await registerClient(client, wallet);
+  if (urlToGroupId[url] == undefined) {
+    // if a group has not been created for url, make it \
+    const conversation = await client.conversations.newConversation([
+    ]);
+    urlToGroupId[url] = conversation.id
+  }
+    const convo = await client.conversations.getConversationById(urlToGroupId[url])
+  convo.send(messageContent)
 }
 
 // Function to create a wallet from a private key
@@ -39,10 +63,31 @@ async function createWallet() {
   return wallet;
 }
 
+
+/**const wallet = await createWallet();
+    // Set up the XMTP client with the wallet and database path
+    if (!fs.existsSync(`.cache`)) {
+      fs.mkdirSync(`.cache`);
+    }
+  const client = await setupClient(wallet, {
+      dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+  });
+      await registerClient(client, wallet);
+
+  if (urlToGroupId[url] == undefined) {
+    // if a group has not been created for url, make it \
+    const conversation = await client.conversations.newConversation([
+    ]);
+    urlToGroupId[url] = conversation.id
+  }
+  const convo = await client.conversations.getConversationById(urlToGroupId[url])
+  //await convo.send("lmfao") */
+
+
 // Function to create and setup the XMTP client
 async function setupClient(wallet, config = {}) {
   let initialConfig = {
-    env: "production",
+    env: "dev",
   };
   const finalConfig = { ...initialConfig, ...config };
 
@@ -54,6 +99,8 @@ async function setupClient(wallet, config = {}) {
 // Function to register the client if not already registered
 async function registerClient(client, wallet) {
   if (!client.isRegistered) {
+        
+
     const signature = toBytes(
       await wallet.signMessage({
         message: client.signatureText,
@@ -112,10 +159,137 @@ async function main() {
   await registerClient(client, wallet);
   // Handle existing conversations
   await handleConversations(client);
+
   // Run message streaming in a parallel thread to respond to new messages
   (async () => {
     await streamAndRespond(client);
   })();
 }
 // Example usage
-main();
+
+async function createConvoTest(url) {
+  if (urlToGroupId[url] == undefined) {
+    const wallet = await createWallet();
+    // Set up the XMTP client with the wallet and database path
+    if (!fs.existsSync(`.cache`)) {
+      fs.mkdirSync(`.cache`);
+    }
+    const client = await setupClient(wallet, {
+      dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+    });
+    // Register the client with the XMTP network if not already registered
+    await registerClient(client, wallet);
+    const conversation = await client.conversations.newConversation([
+    ]);
+    console.log(conversation.id);
+
+    urlToGroupId[url] = conversation.id
+    
+    return conversation.id
+  }
+  //else
+  console.log("already exists a group id for that url")
+  return urlToGroupId[url]
+}
+
+async function test() {
+  console.log(await createConvoTest("chatgpt.com"))
+}
+
+async function run() {
+  const wallet = await createWallet();
+    // Set up the XMTP client with the wallet and database path
+    if (!fs.existsSync(`.cache`)) {
+      fs.mkdirSync(`.cache`);
+    }
+    const client = await setupClient(wallet, {
+      dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+    });
+  
+  await sendMessageToGroup(client, 'fe2200f01ad87a7576d363a1db800518', "gm");
+      console.log((await client.conversations.getConversationById('fe2200f01ad87a7576d363a1db800518')).messages())
+
+}
+
+
+async function getMessagesByUrl(url) {
+  const wallet = await createWallet();
+    // Set up the XMTP client with the wallet and database path
+    if (!fs.existsSync(`.cache`)) {
+      fs.mkdirSync(`.cache`);
+    }
+  const client = await setupClient(wallet, {
+      dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+  });
+  await registerClient(client, wallet);
+
+  if (urlToGroupId[url] == undefined) {
+    // if a group has not been created for url, make it \
+    const conversation = await client.conversations.newConversation([
+    ]);
+    console.log("creating new group")
+    urlToGroupId[url] = conversation.id
+  }
+  const convo = await client.conversations.getConversationById(urlToGroupId[url])
+  //await convo.send("lmfao")
+  return convo.messages()
+  
+}
+
+
+app.use(cors({
+  origin: 'http://localhost:3000', // Allow only your frontend URL
+  methods: 'GET,POST,PUT,DELETE,OPTIONS', // Allow specific HTTP methods
+  allowedHeaders: 'Content-Type,Authorization' // Allow specific headers
+}));
+
+// Middleware
+app.use(express.json());
+
+// Route handling string input via query parameter
+app.get('/api/fetch', async (req, res) => {
+  const url = req.query.url;
+  if (req.query.msg) {
+    try {
+      const msg = req.query.msg
+      console.log('received msg send rq:' + req.query.msg)
+      console.log("url:" + req.query.url)
+      const wallet = await createWallet();
+      // Set up the XMTP client with the wallet and database path
+      if (!fs.existsSync(`.cache`)) {
+        fs.mkdirSync(`.cache`);
+      }
+      const client = await setupClient(wallet, {
+        dbPath: `.cache/${wallet.account?.address}-${"prod"}`,
+      });
+      await registerClient(client, wallet);
+      const convo = await client.conversations.getConversationById(urlToGroupId[url])
+      console.log(convo)
+      await convo.send(msg)
+      res.json("successfully sent message")
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: 'An unexpected error occurred.' });
+      // test
+    }
+  } else {
+    try {
+    
+      console.log("received a request!");
+       // Access query parameter 'url'
+      if (typeof url === 'string') {
+        const data = await getMessagesByUrl(url);
+        res.json(data);
+      } else {
+        res.status(400).json({ error: 'url query parameter is required and must be a string.' });
+      }
+    } catch (error) {
+      console.error(error);
+      res.status(400).json({ error: 'An unexpected error occurred.' });
+    }
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Backend server running on http://localhost:${port}`);
+});
